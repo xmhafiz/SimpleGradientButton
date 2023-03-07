fad60be6bc911313163673363b796eee845f78df
Mohd Hafiz <xmhafiz@gmail.com>
2023-03-08 03:08:35 +0800

    updated view controller to demo on prog adding button

--- GradienButtonApp/ViewController.swift
+++ GradienButtonApp/ViewController.swift
@@ -7,6 +7,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Add GradientButton programatically
+        let gradientButton = GradientButton()
+        gradientButton.startColor = UIColor.red
+        gradientButton.endColor = UIColor.yellow
+        gradientButton.setTitle("Button", for: .normal)
+        // Set constraint height
+        gradientButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        // Add the button to the stack view
+        contentStackView.addArrangedSubview(gradientButton)
     }
 }
 
