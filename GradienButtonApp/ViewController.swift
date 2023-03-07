c50e46568c7c0e0aba273410ecfd2520b2615d54
Mohd Hafiz <xmhafiz@gmail.com>
2023-03-08 03:08:04 +0800

    added GradientButton class

--- GradienButtonApp/ViewController.swift
+++ GradienButtonApp/ViewController.swift
@@ -1,19 +1,12 @@
-//
-//  ViewController.swift
-//  GradienButtonApp
-//
-//  Created by Mohd Hafiz on 07/03/2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    // After connected from storyboard to this class
+    @IBOutlet weak var contentStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
